74af7663bda65ef1aa5f1e00f2e3a7bbd9fd781c
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-11-26 23:06:34 -0600

    My first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Created 11-26-2024 by Keith Roberts -->
+    <!-- This page is will be the homepage for my business -->
+    <title>Jellies</title>
+    <link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+
+<!-- Navigation Menu -->
+<div id="NavContainer">
+    <a href="index.php">Home</a>
+    <a href="products.php">Products</a>
+    <a href="about.php">About Us</a>
+    <a href="contact.php">Contact</a>
+</div>
+
+<hr/>
+
+<!--Title-->
+<h1>Welcome to Jellies!</h1>
+<h2>Your source of jellyfish and jellyfish aquariums.</h2>
+
+
+<!-- Business Description -->
+<h2>About Us</h2>
+<p>At Jellies we offer a variety of jellyfish and aquariums.</p>
+
+<!-- Picture -->
+<img src="images/jellyfish_image.jpg" alt="Jellyfish" width="600"  />
+
+</body>
+</html>
