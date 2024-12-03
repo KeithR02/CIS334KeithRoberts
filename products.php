72dff088fe497f8aa8eb34125257bf02a3cbfec4
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-12-03 09:18:59 -0600

    Added arrays for my products page

---
+++ products.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Created 12-03-2024 by Keith Roberts -->
+<title>Products</title>
+</head>
+<body>
+<h1>Jellies Products</h1>
+<hr />
+<?php //Jellyfish product arrays (name, price, country of origin, and size)
+$BlueJellyfish = array("Blue Jelly", "56.99", "USA", "Large");
+$GreenJellyfish = array("Green Jelly", "67.74", "UK", "Small");
+$MoonJellyfish = array("Moon Jelly", "63.28", "Spain", "Large");
+$RedJellyfish = array("Red Jelly", "93.52", "Australia", "Medium");
+$PurpleJellyfish = array("Purple Jelly", "48.66", "Mexico", "Small");
+
+?>
+</body>
+</html>
