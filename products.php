a7011a3d85c2baf54519046914801feb1b29e6fd
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-12-03 09:45:20 -0600

    Added a table for my products and comments.

--- products.php
+++ products.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <!-- Created 12-03-2024 by Keith Roberts -->
-<title>Products</title>
+<title>Products</title>\
+    <link rel="stylesheet" type="text/css" href="style.css"
 </head>
 <body>
 <h1>Jellies Products</h1>
@@ -13,7 +14,45 @@ $GreenJellyfish = array("Green Jelly", "67.74", "UK", "Small");
 $MoonJellyfish = array("Moon Jelly", "63.28", "Spain", "Large");
 $RedJellyfish = array("Red Jelly", "93.52", "Australia", "Medium");
 $PurpleJellyfish = array("Purple Jelly", "48.66", "Mexico", "Small");
-
 ?>
+
+<table>
+    <tr>
+        <th>Jellyfish Type</th>
+        <th>Price</th>
+        <th>Country of Origin</th>
+        <th>Jellyfish Size</th>
+    </tr>
+    <tr>
+        <td><?php echo $BlueJellyfish[0]; ?></td>
+        <td><?php echo $BlueJellyfish[1]; ?></td>
+        <td><?php echo $BlueJellyfish[2]; ?></td>
+        <td><?php echo $BlueJellyfish[3]; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo $GreenJellyfish[0]; ?></td>
+        <td><?php echo $GreenJellyfish[1]; ?></td>
+        <td><?php echo $GreenJellyfish[2]; ?></td>
+        <td><?php echo $GreenJellyfish[3]; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo $MoonJellyfish[0]; ?></td>
+        <td><?php echo $MoonJellyfish[1]; ?></td>
+        <td><?php echo $MoonJellyfish[2]; ?></td>
+        <td><?php echo $MoonJellyfish[3]; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo $RedJellyfish[0]; ?></td>
+        <td><?php echo $RedJellyfish[1]; ?></td>
+        <td><?php echo $RedJellyfish[2]; ?></td>
+        <td><?php echo $RedJellyfish[3]; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo $PurpleJellyfish[0]; ?></td>
+        <td><?php echo $PurpleJellyfish[1]; ?></td>
+        <td><?php echo $PurpleJellyfish[2]; ?></td>
+        <td><?php echo $PurpleJellyfish[3]; ?></td>
+    </tr>
+</table>
 </body>
 </html>
