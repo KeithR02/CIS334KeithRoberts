bb7ee270d5cde7fb75aca6c0689c20587064de33
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-12-10 06:15:42 -0600

    Added part 1 instuctions, a new file called inc welcome, two variables called login and username, and a if statement to check login status.

---
+++ inc_welcome.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Created 12-10-2024 by Keith Roberts -->
+    <!-- -->
+    <title>Jellies</title>
+    <link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+
+<!-- Navigation Menu -->
+<!--<div id="NavContainer">-->
+<!--    <a href="index.php">Home</a>-->
+<!--    <a href="products.php">Products</a>-->
+<!--    <a href="about.php">About Us</a>-->
+<!--    <a href="contact.php">Contact</a>-->
+<!--</div>-->
+
+<!--<hr/>-->
+
+<!--Title-->
+<h1>Check login status</h1>
+
+<!--Created two variables, one for login and one for username and an if statement to check login-->
+    <?php
+        $isLogin = false;
+
+        $username = null;
+
+        if ($isLogin) {
+            echo "Welcome, $username!";
+        } else {
+            echo "Please log in to continue.";
+        }
+    ?>
+
+</body>
+</html>
