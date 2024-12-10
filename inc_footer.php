20b75c34a35e43aac9ddce24e5b9fb4cb0e297d3
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-12-10 06:51:36 -0600

    Added a footer file, a footer with the copyright logo, current year, and my name.

---
+++ inc_footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Created 12-10-2024 by Keith Roberts -->
+    <title>Jellies</title>
+    <link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+
+<!--This gets the current year for the copyright-->
+<?php
+$currentYear = date("Y");
+?>
+
+<!-- Footer -->
+<footer>
+    <p>&copy; <?php echo $currentYear; ?> Keith Roberts.</p>
+</footer>
+
+</body>
+</html>
