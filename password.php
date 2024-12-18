8f8cdf6e7fba6e009206fab00daa7f21dd553ddb
Keith Roberts <keith_roberts@stu.indianhills.edu>
2024-12-17 19:57:22 -0600

    Added an array of 5 good passwords, and 5 bad passwords

---
+++ password.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Created 12-17-2024 by Keith Roberts -->
+    <title>Jellies</title>
+    <link rel="stylesheet" type="text/css" href="style.css"
+</head>
+<body>
+
+<!--An array of 10 passwords, 5 good and 5 bad.-->
+<?php
+$Products = array(
+    "s7%feL1wxTc", //good passwords
+    "g2-bru4Life5",
+    "eC3bri#-dvrv",
+    "x4ihf+e6F4Eq",
+    "d531_fPur#62",
+
+    "password", //bad passwords
+    "i like cats",
+    "jeffery",
+    "12345",
+    "password1"
+);
+?>
+
+
+</body>
+</html>
